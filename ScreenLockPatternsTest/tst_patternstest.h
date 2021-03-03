ef2338487c3ce44a852a9beb4662c604da2e026d
SandroWissmann <sandro.wissmann@gmail.com>
2021-03-03 17:01:23 +0100

    test: fixxes wrong test cases

--- ScreenLockPatternsTest/tst_patternstest.h
+++ ScreenLockPatternsTest/tst_patternstest.h
@@ -32,70 +32,43 @@ TEST(Patterns, length1)
 TEST(Patterns, length2)
 {
     constexpr auto length = 2;
-    constexpr auto expectedResult = 5;
 
-    EXPECT_EQ(countPatternsFrom('A', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('B', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('C', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('D', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('E', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('F', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('G', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('H', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('I', length), expectedResult);
+    EXPECT_EQ(countPatternsFrom('A', length), 5);
+    EXPECT_EQ(countPatternsFrom('B', length), 7);
+    EXPECT_EQ(countPatternsFrom('C', length), 5);
+    EXPECT_EQ(countPatternsFrom('D', length), 7);
+    EXPECT_EQ(countPatternsFrom('E', length), 8);
+    EXPECT_EQ(countPatternsFrom('F', length), 7);
+    EXPECT_EQ(countPatternsFrom('G', length), 5);
+    EXPECT_EQ(countPatternsFrom('H', length), 7);
+    EXPECT_EQ(countPatternsFrom('I', length), 5);
 }
 
 TEST(Patterns, length3)
 {
     constexpr auto length = 3;
-    constexpr auto expectedResult = 37;
 
-    EXPECT_EQ(countPatternsFrom('A', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('B', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('C', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('D', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('E', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('F', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('G', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('H', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('I', length), expectedResult);
+    EXPECT_EQ(countPatternsFrom('D', length), 37);
 }
 
 TEST(Patterns, length4)
 {
     constexpr auto length = 4;
-    constexpr auto expectedResult = 256;
 
-    EXPECT_EQ(countPatternsFrom('A', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('B', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('C', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('D', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('E', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('F', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('G', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('H', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('I', length), expectedResult);
+    EXPECT_EQ(countPatternsFrom('E', length), 256);
 }
 
 TEST(Patterns, length8)
 {
     constexpr auto length = 8;
-    constexpr auto expectedResult = 23280;
 
-    EXPECT_EQ(countPatternsFrom('A', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('B', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('C', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('D', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('E', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('F', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('G', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('H', length), expectedResult);
-    EXPECT_EQ(countPatternsFrom('I', length), expectedResult);
+    EXPECT_EQ(countPatternsFrom('E', length), 23280);
 }
 
 TEST(Patterns, toLong)
 {
-    EXPECT_EQ(countPatternsFrom('A', 0), 0);
+    EXPECT_EQ(countPatternsFrom('A', 8), 0);
+    EXPECT_EQ(countPatternsFrom('A', 10), 0);
 }
 
 #endif // TST_PATTERNSTEST_H
